62863294c98669861a5f50636ae13080693274a6
Rodrigo <rodrigolima.rdl@gmail.com>
2023-11-07 21:54:27 -0300

    commit do dia 07.11

---
+++ 11.03.2023/Exercicio 1/main.dart
@@ -0,0 +1,15 @@
+int soma(int a, int b) {
+
+  return a + b;
+}
+void main () {
+
+  int numero1 = 8;
+
+  int numero2 = 7;
+
+  int resultado = soma (numero1 , numero2);
+
+print("A soma de $numero1 e $numero2 é $resultado.");
+
+}
